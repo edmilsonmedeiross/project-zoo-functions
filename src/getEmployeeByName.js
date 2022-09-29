fcee1cc95b005b36cc60fcc6172da91b031c679d
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-09-29 16:43:05 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) { return {}; }
+  return data.employees.find((person) => person.firstName === employeeName
+  || person.lastName === employeeName);
 }
+
+/* console.log(getEmployeeByName('Nelson')); */
 
 module.exports = getEmployeeByName;
