3d9862b007fe96c0d432fbaff8cb3271fd8349d0
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-09-29 15:41:58 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => species.filter((animal) => ids.some((id) => animal.id === id));
+// retorna o animal correspondente ao id ou ids se for verdadeiro.
+
+/* console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46',
+  'baa6e93a-f295-44e7-8f70-2bcdc6f6948d')); */
 
 module.exports = getSpeciesByIds;
