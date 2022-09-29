61af122797a21d2aa88715bcb92de7f13d3dc4f8
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-09-29 20:31:46 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((person) => person.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    console.log('foi');
+    return employees.filter((person) => person.managers.some((id) => id.includes(managerId)))
+      .map((person) => `${person.firstName} ${person.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+/* console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83')); */
 
 module.exports = { isManager, getRelatedEmployees };
