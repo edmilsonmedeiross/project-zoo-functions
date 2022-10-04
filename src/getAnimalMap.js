fdcad231dc0e820c99bf60d28c129ca42753ca5c
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-10-04 15:45:50 -0300

    requisito 9 e 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,50 @@
-const data = require('../data/zoo_data');
+const { data, species } = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const animalAnimal = (includeNames, sorted, sex) => {
+  const objAnimal = species.find((specie) => specie.name === includeNames)
+    .residents.filter((animal) => {
+      if (sex) return animal.sex === sex;
+      return animal;
+    }).map((animal) => animal.name);
+  if (sorted) return objAnimal.sort();
+  return objAnimal;
+};
 
+/* console.log(animalAnimal({ includeNames: true })); */
+
+const animalNames = (includeNames, sorted, sex) => {
+  const regions = ['NE', 'NW', 'SE', 'SW'];
+
+  return regions.reduce((acc, region) => {
+    const aniNames = {};
+
+    const regionSpecies = includeNames[region].map((specie) => {
+      const newObj = {};
+      newObj[specie] = animalAnimal(specie, sorted, sex);
+      return newObj;
+    });
+    aniNames[region] = regionSpecies;
+
+    return Object.assign(acc, aniNames);
+  }, {});
+};
+
+const getAnimalMap = (options) => {
+  const regions = ['NE', 'NW', 'SE', 'SW'];
+
+  const includeNames = regions.reduce((acc, region) => {
+    acc[region] = species.filter((specie) => specie.location === region)
+      .map((specie) => specie.name);
+
+    return acc;
+  }, {});
+
+  if (options && options.includeNames) {
+    return animalNames(includeNames, options.sorted, options.sex);
+  }
+  return includeNames;
+};
+
+/* getAnimalMap();
+ */
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
-const data = require('../data/zoo_data');
+const { data, species, hours } = require('../data/zoo_data');
+
+const week = Object.keys(hours);
+/* console.log(week); */
+
+const workLog = week.reduce((acc, chave) => {
+  acc[chave] = {
+    officeHour: `Open from ${hours[chave].open}am until ${hours[chave].close}pm`,
+    exhibition: species.filter((animal) =>
+      animal.availability.includes(chave)).map((specie) => specie.name),
+  };
+  return acc;
+}, {});
+
+workLog.Monday = {
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!',
+};
+
+console.log((workLog));
+
+/* const ani = Object.entries(workLog); */
+/* console.log(ani); */
+
+/* const zzz = ani.find((dia) => dia.includes('Tuesday'));
+console.log(zzz); */
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) return workLog;
+
+  const validate = species.some((animal) => animal.name === scheduleTarget); // valida se algum animal foi passado como parametro
+
+  if (!week.includes(scheduleTarget) && !validate) return workLog;
+
+  if (week.includes(scheduleTarget)) return { [scheduleTarget]: workLog[scheduleTarget] };
+
+  return species.find((animali) => animali.name === scheduleTarget).availability;
 }
 
+/* console.log(getSchedule('supimpa')); */
 module.exports = getSchedule;
